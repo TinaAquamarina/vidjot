c22dbee4c106bd47b0657be9c98ade39f309836b
Tina <tina@Tinas-MacBook-Pro.local>
2018-10-17 10:19:45 +0200

    Add styling, connect database

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-const express = require('express'); // require express
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
-const app = express(); // initialize app
+const app = express();
 
-const port = 5000; // set up port
+// Connect to mongoose
+mongoose.connect('mongodb://127.0.0.1:27017/vidjot-dev', {useNewUrlParser: true})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
-app.listen();
+// Handlebars middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Index route
+app.get('/', (req, res) => {
+  console.log(req.name);
+  res.render('index');
+});
+
+// About route
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
