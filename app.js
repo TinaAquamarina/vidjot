52cc4f7cd7ecf11f42ebf918a3ef08e639d8723e
Tina <tina@tinas-mbp.intern>
2018-10-23 13:06:50 +0200

    Add POST route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -19,6 +20,10 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+// Add body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 // ROUTES
 // Index route
 app.get('/', (req, res) => {
@@ -34,7 +39,36 @@ app.get('/about', (req, res) => {
 // Add idea form
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add')
-})
+});
+
+// POST process form
+app.post('/ideas', (req, res) => {
+  let errors = [];
+
+  if(!req.body.title){
+    errors.push({text:'Please add a title'});
+  }
+  if(!req.body.details){
+    errors.push({text:'Please add some details'});
+  }
+  if(errors.length > 0) {
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    })
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    }
+    new Idea(newUser)
+    .save()
+    .then(idea => {
+      res.redirect('/ideas');
+    })
+  }
+});
 
 const port = 5000;
 
